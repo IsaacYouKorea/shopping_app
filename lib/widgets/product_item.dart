9c2ba6643a70136bb628bff960dd2830a89deaaf
isaac_you <isaac_you@mhand.net>
2019-10-23 02:18:02 +0900

    * font 추가, Theme, ClipRRect를 이용한 border-radius 적용

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,19 +8,27 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        product.imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        title: Text(product.title, textAlign: TextAlign.center),
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite_border),
-          onPressed: () => {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
         ),
-        trailing: IconButton(icon: Icon(Icons.add_shopping_cart), onPressed: () => {},),
+        footer: GridTileBar(
+          title: Text(product.title, textAlign: TextAlign.center),
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite_border),
+            onPressed: () => {},
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.add_shopping_cart),
+            onPressed: () => {},
+            color: Theme.of(context).accentColor,
+          ),
+        ),
       ),
     );
   }
